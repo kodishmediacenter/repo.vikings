f364e69a711ae8cb078a020c9d2fd2cd1b69f736
vikingsbuild <37523732+vikingsbuild@users.noreply.github.com>
2019-01-20 20:41:52 -0200

    update addons

--- plugin.video.AladoTV/default.py
+++ plugin.video.AladoTV/default.py
@@ -2161,11 +2161,11 @@ def sendJSON( command):
 def SetViewThumbnail():
     skin_used = xbmc.getSkinDir()
     if skin_used == 'skin.confluence':
-        xbmc.executebuiltin('Container.SetViewMode(500)')
+        xbmc.executebuiltin('Container.SetViewMode(55)')
     elif skin_used == 'skin.aeon.nox':
         xbmc.executebuiltin('Container.SetViewMode(511)') 
     else:
-        xbmc.executebuiltin('Container.SetViewMode(500)')
+        xbmc.executebuiltin('Container.SetViewMode(50)')
 	
 	
 def pluginquerybyJSON(url):
